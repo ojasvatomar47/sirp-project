44e0c3aff77bb9920277ab862caa3cdbd9bb0e3d
Ojasva Tomar <ojasvatomar47@gmail.com>
2023-10-31 21:11:10 +0530

    Homepage backend setup

--- frontend/src/components/Notice.jsx
+++ frontend/src/components/Notice.jsx
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../context/authContext';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SectionWithScrollableCards = () => {
   const [data1, setData1] = useState([
@@ -17,12 +20,12 @@ const SectionWithScrollableCards = () => {
       date: '2023-10-27',
     },
     {
-        id: 2,
-        title: 'Card 2',
-        content: 'Content for Card 2',
-        postedBy: 'Caretaker 2',
-        date: '2023-10-27',
-      },
+      id: 2,
+      title: 'Card 2',
+      content: 'Content for Card 2',
+      postedBy: 'Caretaker 2',
+      date: '2023-10-27',
+    },
   ]);
 
   const [data2, setData2] = useState([
@@ -41,44 +44,117 @@ const SectionWithScrollableCards = () => {
       date: '2023-10-29',
     },
     {
-        id: 4,
-        title: 'Card 4',
-        content: 'Content for Card 4',
-        postedBy: 'Warden 2',
-        date: '2023-10-29',
-      },
+      id: 4,
+      title: 'Card 4',
+      content: 'Content for Card 4',
+      postedBy: 'Warden 2',
+      date: '2023-10-29',
+    },
   ]);
+
+  const { currentUser, logout } = useContext(AuthContext)
+
+  const [complaints, setComplaints] = useState([])
+
+  const [notices, setNotices] = useState([])
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+
+    if (currentUser) {
+
+      const { role, hostel_name, student_id } = currentUser
+
+      const fetchComplaints = async () => {
+        try {
+          const res = await axios.get(`http://localhost:8800/api/complain`, { params: { hostel: hostel_name } })
+          setComplaints(res.data)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+
+      fetchComplaints()
+
+    }
+  }, [currentUser])
+
+  useEffect(() => {
+
+    if (currentUser) {
+
+      const { hostel_name } = currentUser
+
+      const fetchNotices = async () => {
+        try {
+          const res = await axios.get(`http://localhost:8800/api/notice`, { params: { hostel: hostel_name } })
+          setNotices(res.data)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+
+      fetchNotices()
+
+    }
+  }, [currentUser])
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+
+    try {
+      await logout(currentUser.role)
+      console.log("User has been logged out successfully")
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const formatSubmissionDateTime = (dateTimeString) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const date = new Date(dateTimeString).toLocaleDateString(undefined, options);
+    const time = new Date(dateTimeString).toLocaleTimeString();
+    return `${date} ${time}`;
+  };
+
 
   return (
     <div className="flex h-96 "> {/* Increased container height to h-96 */}
-      <div className="w-1/2 h-96 p-2 overflow-y-auto bg-gray-300">
-        <h2 className="text-center text-2xl mb-2 text-black"><b>NOTICES</b></h2>
-        {data1.map((card) => (
-          <div key={card.id} className="mb-4 p-4 border rounded shadow flex flex-col justify-between h-50 bg-white">
-            <div className="text-center">
-              <h3 className="text-lg font-semibold">{card.title}</h3>
-              <p className="text-gray-600">Description: {card.content}</p>
-            </div>
-            <div className="text-center">
-              <p className="text-black">Posted by: {card.postedBy}</p>
-              <p className="text-black">Date: {card.date}</p>
-            </div>
+      <div className="w-1/2 h-96 p-2 overflow-y-auto">
+        <h2 className="text-center text-2xl mb-2 text-black"><b>COMPLAINTS</b></h2>
+        {complaints.map((complaint) => (
+          <div key={complaint.complaint_id} className="mb-4 p-4 border rounded shadow flex flex-col justify-between h-50 bg-white">
+            <Link to={`/complaint/:${complaint.complaint_id}`}>
+              <div className="text-center">
+                <h3 className="text-lg font-semibold">{complaint.title}</h3>
+                <p className="text-gray-600">{complaint.description}</p>
+              </div>
+              <div className="text-center">
+                <p className="text-black">Posted by: {complaint.student_username}</p>
+                <p className="text-black">Date: {formatSubmissionDateTime(complaint.submission_date)}</p>
+              </div>
+            </Link>
           </div>
         ))}
       </div>
 
-      <div className="w-1/2 h-96 p-2 overflow-y-auto mr-2 bg-gray-300">
-        <h2 className="text-center text-2xl mb-2 text-black"><b>RECENT</b></h2>
-        {data2.map((card) => (
-          <div key={card.id} className="mb-4 p-4 border rounded shadow flex flex-col justify-between h-55 bg-white">
-            <div className="text-center">
-              <h3 className="text-lg font-semibold">{card.title}</h3>
-              <p className="text-gray-600">Description: {card.content}</p>
-            </div>
-            <div className="text-center">
-              <p className="text-gray-600">Posted by: {card.postedBy}</p>
-              <p className="text-gray-600">Date: {card.date}</p>
-            </div>
+      <div className="w-1/2 h-96 p-2 overflow-y-auto mr-2">
+        <h2 className="text-center text-2xl mb-2 text-black"><b>NOTICES</b></h2>
+        {notices.map((notice) => (
+          <div key={notice.notice_id} className="mb-4 p-4 border rounded shadow flex flex-col justify-between h-55 bg-white">
+            <Link to={`/notice/:${notice.notice_id}`}>
+              <div className="text-center">
+                <h3 className="text-lg font-semibold">{notice.title}</h3>
+                <p className="text-gray-600">{notice.content}</p>
+              </div>
+              <div className="text-center">
+                <p className="text-gray-600">Posted by: {notice.postedBy}</p>
+                <p className="text-gray-600">Date: {notice.user_role === 'Caretaker' ? "Caretaker" : "Warden"}</p>
+                <p className="text-black">Date: {formatSubmissionDateTime(notice.date)}</p>
+              </div>
+            </Link>
           </div>
         ))}
       </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,90 +1,14 @@
-// import React from 'react'
-
-// const Home = () => {
-//   return (
-//     <div>Home</div>
-//   )
-// }
-
-// export default Home
-
 import React from 'react' ;
 import Hero from '../components/Hero' ;
 import Services from '../components/Services' ;
 import Container from '../components/Container' ;
 import Instruct from '../components/Instruct';
 import Notice from '../components/Notice'
+
 const Home = () => {
 
-  const { currentUser, logout } = useContext(AuthContext)
-
-  const [complaints, setComplaints] = useState([])
-
-  const [notices, setNotices] = useState([])
-
-  const navigate = useNavigate()
-
-  useEffect(() => {
-
-    if (currentUser) {
-
-      const { role, hostel_name, student_id } = currentUser
-
-      const fetchComplaints = async () => {
-        try {
-          const res = await axios.get(`http://localhost:8800/api/complain`, { params: { hostel: hostel_name } })
-          setComplaints(res.data)
-        } catch (error) {
-          console.log(error)
-        }
-      }
-
-      fetchComplaints()
-
-    }
-  }, [currentUser])
-
-  useEffect(() => {
-
-    if (currentUser) {
-
-      const { hostel_name } = currentUser
-
-      const fetchNotices = async () => {
-        try {
-          const res = await axios.get(`http://localhost:8800/api/notice`, { params: { hostel: hostel_name } })
-          setNotices(res.data)
-        } catch (error) {
-          console.log(error)
-        }
-      }
-
-      fetchNotices()
-
-    }
-  }, [currentUser])
-
-  const handleLogout = async (event) => {
-    event.preventDefault()
-
-    try {
-      await logout(currentUser.role)
-      console.log("User has been logged out successfully")
-      navigate('/')
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  const formatSubmissionDateTime = (dateTimeString) => {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    const date = new Date(dateTimeString).toLocaleDateString(undefined, options);
-    const time = new Date(dateTimeString).toLocaleTimeString();
-    return `${date} ${time}`;
-  };
-
   return (
-    <div>
+    <div className='bg-white'>
       <Hero /> 
       <Instruct/>
       <Services /> 
